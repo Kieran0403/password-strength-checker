18529a12df5b956f997cebc30782e3f054d3c150
Kieran0403 <kieran0403@icloud.com>
2026-06-11 10:46:11 +0100

    Added some more helper methods

--- password_checker.py
+++ password_checker.py
@@ -1,3 +1,4 @@
+import string,re
 class PasswordChecker:
     def __init__(self,password):
         """Constructor to initialise the user's password to a variable
@@ -10,10 +11,32 @@ class PasswordChecker:
     def check_length(self,password):
         """Method to find the length of a users password
 
-        The longer the password is, the more secure they tend to be
+        The longer the password is, the more secure they tend to be, so more points scored
 
         Args:
             password (string): users password
         """
-        length=len(self.password)
+        length=len(self)
+        if length >= 16:
+            return 2  # bonus point for very long passwords
+        elif length >= 8:
+            return 1
+        else:
+            return 0
 
+    def check_special_chars(self):
+        """Checks if the password contains any special characters
+
+        Returns:
+            boolean: if contains special chars
+        """
+        special_chars=string.punctuation #All the special chars from import
+        return special_chars in self.password
+            
+
+    def check_uppercase(self):
+        return bool(re.search(r'[A-Z]', self.password))
+    def check_lowercase(self):
+        return bool(re.search(r'[a-z]', self.password))
+    def check_numbers(self):
+        return 
